320bb516f1ec4ce9418ad684448c04df4f63d8e1
Giovanni Vicario <giovanni.vic@hotmail.com>
2025-02-22 07:55:32 +0000

    fix turn with wave butn

--- godot/scenes/buttons/btn_turn.gd
+++ godot/scenes/buttons/btn_turn.gd
@@ -3,14 +3,25 @@ extends GameButton
 var finish_event: bool = false
 
 @onready var sprite_button: Sprite2D = $Sprite2D/Sprite_Button
+@onready var disp_wave: Node2D = $"../disp_wave"
+
+var maxRot = 249
+var gotRight = false
+
+func _ready() -> void:
+	disp_wave.finish_event.connect(gotRightWave)
 
 func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
-	if event.is_action_pressed("click"):
+	if event.is_action_pressed("click") and !gotRight:
 		if(!finish_event):
-			if(sprite_button.rotation_degrees < 210):
-				sprite_button.rotation_degrees += 30
+			if(sprite_button.rotation_degrees < 210 and sprite_button.rotation_degrees + 80 <= maxRot):
+				sprite_button.rotation_degrees += 80
+				disp_wave.changeWave()
 			else:
-				sprite_button.rotation = 249
-				emiteSinal()
-				
-			print("End: ", sprite_button.rotation_degrees)
+				disp_wave.changeWave()
+				sprite_button.rotation = maxRot
+
+func gotRightWave():
+	print('certo')
+	gotRight = true
+	emiteSinal()
--- godot/scenes/displays/disp_wave.gd
+++ godot/scenes/displays/disp_wave.gd
@@ -5,19 +5,35 @@ extends Node2D
 @onready var disp_third_wave: Sprite2D = $DispThirdWave
 @onready var disp_four_wave: Sprite2D = $DispFourWave
 
-var waves = [disp_first_wave, disp_sec_wave, disp_third_wave, disp_four_wave]
-var index = 0
+signal finish_event
 
+var waves: Array[Sprite2D]
+var index = 1
+var randomChose = randi_range(1,3)
+
+func _ready() -> void:
+	waves = [disp_first_wave, disp_sec_wave, disp_third_wave, disp_four_wave]
+# So serve para a primeira vez q liga ele, precisa ter pra segunda
 func changeWave():
-	if(index > 3):
+	if(index > 2):
 		index = 0
-		waves[4].visible = false
-		waves[index].visible = true
+		waves[3].set_visible(false)
+		waves[index].set_visible(true)
 	else:
 		if(index == 0):
-			waves[4].visible = false
-			waves[index].visible = true
+			waves[3].set_visible(false)
+			waves[index].set_visible(true)
 		else:
-			waves[index].visible = true
-			waves[index-1].visible = false
+			waves[index].set_visible(true)
+			waves[index-1].set_visible(false)
 			index += 1
+	if(index == randomChose):
+		finish_event.emit()
+
+func restart():
+	index = 0
+	randomChose = randi_range(1,3)
+	disp_first_wave.set_visible(true)
+	disp_sec_wave.set_visible(false)
+	disp_third_wave.set_visible(false)
+	disp_four_wave.set_visible(false)
