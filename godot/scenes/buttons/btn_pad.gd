8921b96931de8f38e904f6f6defce74ac4657573
Giovanni Vicario <giovanni.vic@hotmail.com>
2025-02-23 08:47:53 +0000

    add new buttons for pad and select

--- godot/scenes/buttons/btn_pad.gd
+++ godot/scenes/buttons/btn_pad.gd
@@ -4,7 +4,6 @@ extends GameButton
 signal pad_finished
 
 @onready var btn_yellow3: TextureButton = $btn_yellow3
-@onready var btn_orange3: TextureButton = $btn_orange3
 @onready var btn_red: TextureButton = $btn_red
 @onready var btn_pink3: TextureButton = $btn_pink3
 @onready var btn_blue4: TextureButton = $btn_blue4
@@ -14,9 +13,22 @@ signal pad_finished
 var enabled: bool = true
 var index = 0
 var finish_event = false
+var pathArray: Array[String]
+
+var yellowPath = "res://art/buttons/btn_pad_yellow.png"
+var redPath = "res://art/buttons/btn_pad_red.png"
+var pinkPath = "res://art/buttons/btn_pad_pink.png"
+var bluePath = "res://art/buttons/btn_pad_blue.png"
+
+func _ready() -> void:
+	pathArray = [
+		"res://art/buttons/btn_pad_red",
+		"res://art/buttons/btn_pad_yellow", 
+		"res://art/buttons/btn_pad_pink", 
+		"res://art/buttons/btn_pad_blue"]
 
 func _process(_delta: float) -> void:
-	if index > 4 && enabled == true:
+	if index > 3 && enabled == true:
 		enabled = false
 		emit_signal("pad_finished")
 
@@ -24,8 +36,8 @@ func _on_area_2d_input_event(_viewport: Node, _event: InputEvent, _shape_idx: in
 	pass
 
 func padControl(namePad: String):
-	const array = ['btn_yellow3', 'btn_orange3', 'btn_red', 'btn_pink3', 'btn_blue4']
-
+	const array = [ 'btn_red', 'btn_yellow3','btn_pink3', 'btn_blue4']
+	
 	#Lembrar q esse materi btn_active_mat é POC pra teste
 	if(array[index] == namePad):
 		index += 1
@@ -38,17 +50,26 @@ func padControl(namePad: String):
 
 #ISSO É GAMBIARRA TIRAR DEPOIS DA NOVA SPRITE
 func showOrder(indexLength):
-	var arrayBtns = [btn_yellow3, btn_orange3, btn_red, btn_pink3, btn_blue4]
+	var arrayBtns = [ btn_red, btn_yellow3, btn_pink3, btn_blue4]
 	var beforeIndex = -2
 	var realIndex = indexLength + 1
 	for i in realIndex:
 		beforeIndex += 1
 		if(beforeIndex >= 0):
-			arrayBtns[beforeIndex].use_parent_material = true
-		if(i < 5):
+			var path = pathArray[beforeIndex] + ".png"
+			var pathHover = pathArray[beforeIndex] + "_active_hover" + ".png"
+			arrayBtns[beforeIndex].texture_normal = ResourceLoader.load(path)
+			arrayBtns[beforeIndex].texture_hover = ResourceLoader.load(pathHover)
+			#arrayBtns[beforeIndex].texture = ResourceLoader.load(arrayBtns[beforeIndex].texture)
+		if(i < 4):
 			#await get_tree().create_timer(0.2).timeout
-			arrayBtns[i].use_parent_material = false
+			var path = pathArray[i] + "_active" + ".png"
+			var pathHover = pathArray[i] + "_active_hover" + ".png"
+			arrayBtns[i].texture_normal = ResourceLoader.load(path)
+			arrayBtns[i].texture_hover = ResourceLoader.load(pathHover)
+			#arrayBtns[i].use_parent_material = false
 	
+	texture_path.insert(sprite_2d.texture.get_path().length() - 4, "_hover")
 	
 func _on_btn_yellow_button_up() -> void:
 	padControl('btn_yellow')
--- godot/scenes/buttons/btn_select.gd
+++ godot/scenes/buttons/btn_select.gd
@@ -7,26 +7,32 @@ signal picked_correct
 @onready var btn_mini_2: TextureButton = $btn_mini_2
 @onready var btn_mini_3: TextureButton = $btn_mini_3
 @onready var btn_mini_4: TextureButton = $btn_mini_4
-@onready var btn_mini_5: TextureButton = $btn_mini_5
 
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 var buttons: Array[TextureButton]
 var chosen: TextureButton
 var button_dict = {
-	"yellow": "btn_mini",
-	"orange": "btn_mini_2",
-	"red": "btn_mini_3",
-	"pink": "btn_mini_4",
-	"blue": "btn_mini_5"
+	"yellow": "btn_mini_2",
+	"red": "btn_mini",
+	"pink": "btn_mini_3",
+	"blue": "btn_mini_4"
 }
+
+var mini_dict = {
+	"btn_mini_2": "res://art/buttons/btn_mini2",
+	"btn_mini": "res://art/buttons/btn_mini",
+	"btn_mini_3": "res://art/buttons/btn_mini3",
+	"btn_mini_4": "res://art/buttons/btn_mini4"
+}
+
 var started: bool = false
 
 func _on_area_2d_input_event(_viewport: Node, _event: InputEvent, _shape_idx: int) -> void:
 	pass
 	
 func _ready() -> void:
-	buttons = [btn_mini, btn_mini_2, btn_mini_3, btn_mini_4, btn_mini_5]
+	buttons = [btn_mini, btn_mini_2, btn_mini_3, btn_mini_4]
 
 func verifyButton(nameButton: String) -> void:
 	audio_stream_player_2d.play()
@@ -40,15 +46,13 @@ func verifyButton(nameButton: String) -> void:
 
 func chooseRandomButton():
 	if(chosen):
-		chosen.use_parent_material = true
+		chosen.texture_normal = ResourceLoader.load(mini_dict[chosen.name] + ".png")
 	chosen = buttons.pick_random()
-	chosen.use_parent_material = false
+	chosen.texture_normal = ResourceLoader.load(mini_dict[chosen.name] + "_active.png")
+	#chosen.use_parent_material = false
 
 func _on_btn_yellow_button_up() -> void:
 	verifyButton("yellow")
-
-func _on_btn_orange_button_up() -> void:
-	verifyButton("orange")
 
 func _on_btn_red_button_up() -> void:
 	verifyButton("red")
