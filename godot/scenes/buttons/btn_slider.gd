f95cf39f687749bb55bd05fb26f3751735463eff
Lucas Rezende de Paula Silva <lucas.rpsilva@hotmail.com>
2025-02-23 02:35:30 -0300

    Slider Max Fix
    Slider para quando está no máximo.

--- godot/scenes/buttons/btn_slider.gd
+++ godot/scenes/buttons/btn_slider.gd
@@ -7,8 +7,10 @@ signal slider_max
 @onready var broken_disp_measurer: Node2D = $"../disp_measurer2"
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
+var slider_at_max: bool = false
+
 func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
-	if event.is_action_pressed("click") and Global.level >= 1:
+	if event.is_action_pressed("click") and Global.level >= 1 and slider_at_max == false:
 		if(sprite_2d.position.y >= -55):
 			sprite_2d.position.y -= 20
 			disp_measurer.update_rating(30) #Is for 6x to reach 180 degree
@@ -16,4 +18,5 @@ func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int
 		else:
 			sprite_2d.position.y = 64
 		if sprite_2d.position.y == -56:
+			slider_at_max = true
 			emit_signal("slider_max")
